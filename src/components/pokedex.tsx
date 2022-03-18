279eba3548205140bafa329249435246153b71d7
swoo1226 <46513548+swoo1226@users.noreply.github.com>
2022-03-18 11:34:47 +0900

    띠부띠부씰 스타일링

--- src/components/pokedex.tsx
+++ src/components/pokedex.tsx
@@ -3,16 +3,71 @@ import { StyleSheet, Text, View, Image, Dimensions, TouchableOpacity, Platform, 
 import pokemonKorean from '../../assets/pokemon/pokemon-korean.json'
 import { useDebounce } from 'use-debounce';
 import {Audio} from 'expo-av'
-import { Center, HStack, useColorModeValue } from 'native-base';
+import {Chip} from 'react-native-paper'
+// import { PokemonTypeColor, PokemonType } from '../utils/color'
+import { Center, HStack, useColorModeValue, Input, Box } from 'native-base';
 //https://docs.nativebase.io/default-theme
 type ImageUri = {
     localUri: string;
     remoteUri?: string | null;
   } | null
 
+// type TransformedPokemonType = keyof PokemonType
+type PokemonTypeColors = {
+  normal: string
+  poison: string
+  psychic: string
+  grass: string
+  ground: string
+  ice: string
+  fire: string
+  rock: string
+  dragon: string
+  water: string
+  bug: string
+  dark: string
+  fighting: string
+  ghost: string
+  steel: string
+  flying: string
+  electric: string
+  fairy: string
+}
+type PokemonColorType = {
+  [key in keyof PokemonTypeColors]: string;
+};
+const PokemonTypeColor: PokemonColorType = {
+  normal: '#bdbdaf',
+  poison: '#a95c9f',
+  psychic: '#f461af',
+  grass: '#8bd54f',
+  ground: '#ebc856',
+  ice: '#97f1ff',
+  fire: '#fa5543',
+  rock: '#ccbc71',
+  dragon: '#8574ff',
+  water: '#56adf3',
+  bug: '#c4d11f',
+  dark: '#7c5f4c',
+  fighting: '#894c3b',
+  ghost: '#736fcd',
+  steel: '#c4c2db',
+  flying: '#79a3ff',
+  electric: '#fee33a',
+  fairy: '#f9adff',
+}
+
+interface Pokemon {
+  id: number
+  name: string
+  imageFront: string
+  imageBack: string
+  type: keyof PokemonTypeColors
+  koreanName: string
+}
 
 export default function Pokedex() {
-    const [pokemon, setPokemon] = useState<any>()
+    const [pokemon, setPokemon] = useState<Pokemon>()
     const [sound, setSound] = useState<any>()
     const [pokemonId, setPokemonId] = useState<string>('1')
     const [value] = useDebounce(pokemonId, 500)
@@ -42,8 +97,9 @@ export default function Pokedex() {
         );
         setSound(sound)
         const pokemon = await data.json()
-        const pokemonType: string = pokemon.types.map((poke: any) => poke.type.name).join(", ")
-        const transformedPokemon = {
+        console.log(pokemon.types)
+        const pokemonType: keyof PokemonTypeColors = pokemon.types.map((info: any) => info.type.name)[0]
+        const transformedPokemon: Pokemon = {
           id: pokemon.id,
           name: pokemon.name,
           imageFront: pokemon.sprites.front_default,
@@ -60,7 +116,6 @@ export default function Pokedex() {
           // front_shiny:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png"
           // front_shiny_female:null
         }
-        console.log(transformedPokemon.type.split(', ')[0])
         setPokemon(transformedPokemon)
       }, [id])
 
@@ -81,9 +136,21 @@ export default function Pokedex() {
       }
 
       const koreanNameChip = () => {
-        if (value) {
+        if (value && pokemon) {
           return (
-            <Text style={styles.sealName}>{value ? `No. ${pokemonNumbering(value)} ${pokemon.koreanName}` : ''}</Text>
+           <HStack style={{alignItems: 'center', borderRadius: 15, borderWidth: 0.5, borderColor: 'gray', alignSelf: 'flex-start'}} shadow={2}>
+             <HStack style={{alignItems: 'center'}}>
+               <Box borderRadius={15} backgroundColor={PokemonTypeColor[pokemon!.type]} pl={2} pr={2}>
+                 {pokemonNumbering(value)}
+                </Box>
+              {/* <Chip style={{backgroundColor: PokemonTypeColor[pokemon!.type]}}>
+                {pokemonNumbering(value)}
+              </Chip> */}
+              <Center pl={1} pr={2} fontWeight='bold'>
+                {pokemon!.koreanName}
+              </Center>
+             </HStack>
+            </HStack>
           )
         }
       }
@@ -111,7 +178,7 @@ export default function Pokedex() {
           {pokemon.koreanName.split('').map((char: string) => <Center h="50" w="50" rounded="md" shadow={2} bg={useColorModeValue('muted.50', 'muted.100')}>{char}</Center>)}
         </HStack>
         }
-        <TextInput style={styles.input} value={pokemonId} placeholder='Please Enter Pokemon ID' onChangeText={(text) => {if(parseInt(text) <= 151) {setPokemonId(text)}}}/>
+        <Input w={250} style={styles.input} value={pokemonId} placeholder='Please Enter Pokemon ID' onChangeText={(text) => {if(parseInt(text) <= 151 || !text) {setPokemonId(text)}}}/>
         <Pressable onPress={playSound} style={styles.cry}>
           <Text>Cry</Text>
         </Pressable>
@@ -119,8 +186,7 @@ export default function Pokedex() {
         <>
         {/* <Image source={{uri: pokemon.image}} style={[StyleSheet.absoluteFill, styles.thumbnail]}/> */}
           <View style={styles.seal}>
-            {/* <Text style={styles.sealName}>{pokemon.name}</Text> */}
-              {koreanNameChip()}
+            {koreanNameChip()}
             <Image source={{uri: pokemon.imageFront}} style={styles.sealImage} resizeMethod='scale' />
           </View>
         </>
---
+++ src/utils/color.ts
@@ -0,0 +1,41 @@
+export interface PokemonTypes {
+    normal: string
+    poison: string
+    psychic: string
+    grass: string
+    ground: string
+    ice: string
+    fire: string
+    rock: string
+    dragon: string
+    water: string
+    bug: string
+    dark: string
+    fighting: string
+    ghost: string
+    steel: string
+    flying: string
+    electric: string
+    fairy: string
+}
+
+export const PokemonTypeColor = {
+    normal: '#bdbdaf',
+    poison: '#a95c9f',
+    psychic: '#f461af',
+    grass: '#8bd54f',
+    ground: '#ebc856',
+    ice: '#97f1ff',
+    fire: '#fa5543',
+    rock: '#ccbc71',
+    dragon: '#8574ff',
+    water: '#56adf3',
+    bug: '#c4d11f',
+    dark: '#7c5f4c',
+    fighting: '#894c3b',
+    ghost: '#736fcd',
+    steel: '#c4c2db',
+    flying: '#79a3ff',
+    electric: '#fee33a',
+    fairy: '#f9adff',
+}
